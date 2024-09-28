8ff0f2b87feeec9065c097024af466db69264077
Prince <princevirmani410@gmail.com>
2024-09-28 15:11:39 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Header />
